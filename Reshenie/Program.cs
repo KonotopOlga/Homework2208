934ad09364114296c900a4a7bd9f2f71d1b09f62
KonotopOlga <ol.konotop@yandex.ru>
2022-08-25 19:09:15 +0300

    решение найдено

---
+++ Reshenie/Program.cs
@@ -0,0 +1,36 @@
+﻿/*
+Пользователь вводит с клавиатуры M чисел. 
+Посчитайте, сколько чисел больше 0 ввёл пользователь.
+0, 7, 8, -2, -2 -> 2
+1, -7, 567, 89, 223-> 3
+*/
+
+
+Console.Write($"Введите число X(количество чисел): ");
+int x = Convert.ToInt32(Console.ReadLine());
+int[] massiveNumbers = new int[x];
+
+void InputNumbers(int x)
+{
+    int i = 0;
+    while (i < x)
+    {
+        Console.Write($"Введите {i + 1} число: ");
+        massiveNumbers[i] = Convert.ToInt32(Console.ReadLine());
+        i++;
+    }
+}
+
+int Comparison(int[] massiveNumbers)
+{
+    int count = 0;
+    for (int i = 0; i < massiveNumbers.Length; i++)
+    {
+        if (massiveNumbers[i] > 0) count += 1;
+    }
+    return count;
+}
+
+InputNumbers(x);
+
+Console.WriteLine($"Чисел больше 0: {Comparison(massiveNumbers)} ");
